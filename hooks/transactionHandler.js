b4f0a1255a2c68cdd7bcd84998c4a459c22d20cf
Deggen <d.kellenschwiler@bsvassociation.org>
2025-08-25 08:22:05 -0500

    fix your outpoint

--- hooks/transactionHandler.js
+++ hooks/transactionHandler.js
@@ -58,7 +58,7 @@ async function spendTransaction(txid, oldThreadInfo, newThreadInfo) {
                 {
                     inputDescription: "Slack thread",
                     unlockingScript: new HashPuzzle().unlock(oldThreadInfo).toHex(),
-                    outpoint: `${oldTx.sourceTXID}.${oldTx.sourceOutputIndex}`,
+                    outpoint: `${oldTx.id('hex')}.0`,
                 }
             ],
             outputs: [
